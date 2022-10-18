70fe845a6eebb7167d7da3e1fd3766995a5c10a2
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-10-18 20:35:15 +0700

    renamed variable name andd add time sleep interval

--- python/i2c.py
+++ python/i2c.py
@@ -2,11 +2,11 @@ import smbus2 as smbus
 import time
 
 bus = smbus.SMBus(1)
-address = 0x04
+SLAVE_ADDRESS = 0x04
 
 while True:
     try:
-        rep = bus.read_i2c_block_data(address, 0)
+        rep = bus.read_i2c_block_data(SLAVE_ADDRESS, 0)
     except:
         continue
 
@@ -14,4 +14,4 @@ while True:
     for i in range(39):
         payload += chr(rep[i])
     print(payload)
-    time.sleep(0.01)
+    time.sleep(1)
