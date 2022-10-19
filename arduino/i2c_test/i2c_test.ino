fcf42f5cbe6205a7447fdb09ed458824cf721d66
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-10-19 15:18:21 +0700

    modified python/i2c and added arduino/i2c

--- arduino/i2c_test/i2c_test.ino
+++ arduino/i2c_test/i2c_test.ino
@@ -8,11 +8,15 @@ void setup() {
   // put your setup code here, to run once:
   Wire.begin(4);                  // Address #0x04
   Wire.onRequest(sendMessage);    // call sendMessage() on request by master
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
 }
 
 void sendMessage() {
--- python/i2c.py
+++ python/i2c.py
@@ -3,15 +3,13 @@ import time
 
 bus = smbus.SMBus(1)
 SLAVE_ADDRESS = 0x04
+MSG_LENGTH = 5          # 5 bytes of data expected
 
 while True:
-    try:
-        rep = bus.read_i2c_block_data(SLAVE_ADDRESS, 0)
-    except:
-        continue
+    rep = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, MSG_LENGTH)
 
     payload = ""
-    for i in range(39):
+    for i in range(5):     # Byte expected
         payload += chr(rep[i])
     print(payload)
     time.sleep(1)
