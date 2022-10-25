13a97454b44576d7f4e650505770aa20d383b789
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-10-25 23:54:52 +0700

    penambahan try catch block

--- python/i2c.py
+++ python/i2c.py
@@ -1,3 +1,4 @@
+import traceback
 import smbus2 as smbus
 import time
 
@@ -6,10 +7,13 @@ SLAVE_ADDRESS = 0x04
 MSG_LENGTH = 5          # 5 bytes of data expected
 
 while True:
-    rep = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, MSG_LENGTH)
-
+    try:
+        rep = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, MSG_LENGTH)
+    except:
+        traceback.print_exception()
+        exit(1)
     payload = ""
-    for i in range(5):     # Byte expected
+    for i in range(MSG_LENGTH):     # Byte expected
         payload += chr(rep[i])
     print(payload)
-    time.sleep(1)
+    time.sleep(0)
