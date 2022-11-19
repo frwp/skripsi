50eca46b234c155ac918e2d78fea768610134a6e
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-11-19 10:35:18 +0700

    update rust, add loop and delay

--- Rust/main_i2c/src/main.rs
+++ Rust/main_i2c/src/main.rs
@@ -16,28 +16,41 @@
  * SDA          ->  A4 (SDA) (dengan pull-up resistor 4.7K ke VCC)
  * SCL          ->  A5 (SCL) (dengan pull-up resistor 4.7K ke VCC)
  *
- * To build this file, run:
- * > cargo build
+ * To build this file, run the script:
+ * > ./build.sh
  *******************************************************************************/
 
- use std::error::Error;
- use std::str;
- 
- use rppal::i2c::I2c;
- 
- const SLAVE_ADDRESS: u16 = 0x04;
- const MESSAGE_LENGTH: usize = 5;
- 
- fn main() -> Result<(), Box<dyn Error>>{
-     let mut i2c = I2c::new()?;
-     let mut buffer: [u8; 5] = [0u8; MESSAGE_LENGTH];
-     
-     // Set the address of the slave
-     i2c.set_slave_address(SLAVE_ADDRESS)?;
- 
-     i2c.read(&mut buffer)?;
-     let payload = str::from_utf8(&buffer).unwrap();
-     println!("{payload}");
-     Ok(())
- }
- 
+use std::error::Error;
+use std::{str, thread, time};
+
+use rppal::i2c::I2c;
+
+const SLAVE_ADDRESS: u16 = 0x04;
+const MESSAGE_LENGTH: usize = 5;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut i2c = I2c::new()?;
+    let mut buffer: [u8; MESSAGE_LENGTH] = [0u8; MESSAGE_LENGTH];
+
+    // Set the address of the slave
+    i2c.set_slave_address(SLAVE_ADDRESS)?;
+
+    loop {
+
+        // get current time for measuring elapsed time during i2c read
+        let time_start = time::Instant::now();
+
+        i2c.read(&mut buffer)?;
+
+        let elapsed = time_start.elapsed();
+        assert!(elapsed <= time::Duration::from_secs(2));
+
+        let payload = str::from_utf8(&buffer).unwrap();
+        println!("{payload}");
+
+        let total_wait = time::Duration::from_secs(2) - elapsed;
+
+        thread::sleep(total_wait);
+    }
+    Ok(())
+}
