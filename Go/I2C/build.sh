fba7e96a86841ec975f6a6f4950d8df3bcc92c5b
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-11-19 09:32:59 +0700

    Update build script

--- Go/I2C/build.sh
+++ Go/I2C/build.sh
@@ -2,7 +2,10 @@
 
 echo "Building package..."
 env GOARCH=arm GOARM=7 GOOS=linux go build -ldflags "-s -w" -a -o main_i2c i2c.go
+mkdir -p target
+mv main_i2c target/main_i2c
 echo "Building done."
+
 echo "Transfering executable to Raspberry Pi..."
 ssh -o ConnectTimeout=1 -q pi@192.168.4.2 exit || ssh -o ConnectTimeout=1 -q pi@192.168.8.141 exit
 if [ $? -ne 0 ]
@@ -10,6 +13,16 @@ then
     echo "Connection failed, is the Raspberry Pi on or has the IP address changed?"
     exit 1
 else
-    scp main_i2c pi@192.168.8.141:~/skripsi/Go/main_i2c
-    echo "Operation done."
+    if command -v rsync &> /dev/null
+    then
+        rsync -P --rsh=ssh $(pwd)/target/main_i2c pi@192.168.8.141:~/skripsi/Go/main_i2c
+    else
+        scp $(pwd)/target/main_i2c pi@192.168.8.141:~/skripsi/Go/main_i2c
+    fi
+    if [ $? -ne 0 ]; then
+        echo "Transfer failed."
+        return 1
+    else
+        echo "Operation done."
+    fi
 fi
--- Rust/main_i2c/build.sh
+++ Rust/main_i2c/build.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
 # Build docker image
+echo "Building docker image..."
 docker build . -t rust_cross_compile/armv7
+echo "Building image done."
 
 # Compile the app using created image
+echo "Building package..."
 docker run --rm -v $(pwd):/app rust_cross_compile/armv7
+echo "Building package done."
 
-# if command -v rsync &> /dev/null
-# then
-#     rsync -P --rsh=ssh 
+echo "Transfering executable to Raspberry Pi..."
+ssh -o ConnectTimeout=1 -q pi@192.168.4.2 exit || ssh -o ConnectTimeout=1 -q pi@192.168.8.141 exit
+if [ $? -ne 0 ]
+then
+    echo "Connection failed, is the Raspberry Pi on or has the IP address changed?"
+    exit 1
+else
+    if command -v rsync &> /dev/null
+    then
+        rsync -P --rsh=ssh $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c pi@192.168.8.141:~/skripsi/Rust/main_i2c
+    else
+        scp $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c pi@192.168.8.141:~/skripsi/Rust/main_i2c
+    fi
+    echo "Operation done."
+fi
