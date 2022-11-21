7b7825b2dd8f52fec81d09beb014ff7c41ab09a5
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-11-21 22:43:08 +0700

    added template/boilerplate

---
+++ dotnet/main_i2c/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.Device.I2c;
+
+
