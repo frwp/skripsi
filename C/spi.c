d12afbc655966b355e62b9a605d81d56f391dd9e
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-12-02 00:47:04 +0700

    working spi code in C

--- C/spi.c
+++ C/spi.c
@@ -2,77 +2,69 @@
  * spi.c
  * SPI master code @ Raspberry Pi in C language
  * @author Fransiskus Rian Wardana Putra
- * 28 Oct 2022
- *
- * Kode ini digunakan untuk tugas akhir S1 Ilmu Komputer dengan topik
- * Bahasa pemrograman dan protokol komunikasi untuk IoT
- *
- * Kode ini menggunakan library WiringPi untuk melakukan setup ke hardware SPI
- * pada perangkat Raspberry Pi. Lihat wiringpi.com dan github.com/WiringPi
- *
- * Perangkat dihubungkan sebagai berikut:
- * (Raspberry Pi)   (Arduino Nano)
- * GND          ->  GND
- * 5V           ->  5V
- * CE0          ->  D10 (SS)
- * MOSI         ->  D11 (COPI)
- * MISO         ->  D12 (CIPO)
- *
- * To build this file, run:
- * > gcc spi.c -lwiringpi -o spi
- */
+ * 2 Dec 2022
+ * For detailed info, wiring, see README.md
+ * build script is included in build.sh
+ * Needs WiringPi library. See wiringpi.com and github.com/WiringPi
+*/
 
 #include <stdio.h>	// print
-#include <stdlib.h> // exit()
-#include <unistd.h> // sleep() and read()
-#include <wiringPi.h>
+#include <unistd.h> // for sleep()
 #include <wiringPiSPI.h>
+#include <time.h>
+
+#define SPI_CHANNEL 0
+#define SPI_CLOCK_SPEED 400000
+#define DATA_LENGTH 42
 
 int main(void)
 {
-	puts("SPI test");
-
-	const int SLAVE_ADDRESS = 0x04;
-	const int MESSAGE_LENGTH = 5;
-
-	// Initiate wiringPi
-	int errno = wiringPiSetup();
-	if (errno < 0)
+	unsigned char buf[1];
+	unsigned char data[50];
+	int fd = wiringPiSPISetupMode(SPI_CHANNEL, SPI_CLOCK_SPEED, 0);
+	if (fd == -1)
 	{
-		printf("setup wiringPi failed: errno %d\n", errno);
-		return errno;
+		puts("Failed to init SPI.");
+		return -1;
 	}
-	puts("wiringPi init success");
+	puts("SPI communication successfully setup.");
 
-	// Initiate spi bus; returns linux file descriptor (fd)
-	int fd = wiringPiSPISetup(SLAVE_ADDRESS);
-	if (fd < 0)
-	{
-		printf("setup wiringPi SPI failed: errno %d\n", fd);
-		return fd;
-	}
-	puts("SPI init success");
+	struct timespec start, stop, sleep_dur; /* for measuring time elapsed */
 
+	puts("Starting SPI transfer.");
 	for (;;)
 	{
-		// Initiate buffer
-		char buf[MESSAGE_LENGTH];
+		timespec_get(&start, TIME_UTC);
 
-		// read the message into the buffer
-		// using linux' read command because wiringpi doesn't support
-		// multiple bytes read and write
-		// see `man 2 read`
-		read(fd, buf, MESSAGE_LENGTH);
+		// Indicates starting point of each transaction.
+		// if slave read 0x10, start SPI transfer at byte position 0.
+		buf[0] = 0x10;
+		wiringPiSPIDataRW(SPI_CHANNEL, buf, 1);
 
-		printf("%s", buf);
-		if (buf[0])
-			printf("\n");
-		if (!buf[0])
+		for (int i = 0; i < DATA_LENGTH; i++) /* 42 is approximated data length */
 		{
-			puts("empty");
-			exit(1);
-		}
-		sleep(2);
+			// dummy byte, discarded on slave.
+			buf[0] = 0x01;
 
-		return 0;
+			// exchange 1 byte with slave
+			wiringPiSPIDataRW(SPI_CHANNEL, buf, 1);
+
+			// buf[0] is populated with data from slave, replacing dummy byte
+			data[i] = buf[0];
+		}
+
+		for (int i = 0; i < DATA_LENGTH; i++)
+		{
+			printf("%c", data[i]);
+		}
+		printf("\n");
+		timespec_get(&stop, TIME_UTC);
+
+		sleep_dur.tv_sec = 2 - (stop.tv_sec - start.tv_sec);
+		sleep_dur.tv_nsec = (stop.tv_nsec - start.tv_nsec) % 1000000000;
+
+		nanosleep(&sleep_dur, NULL);
 	}
+
+	return 0;
+}
