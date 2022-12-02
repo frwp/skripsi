5a77291861278659ed7cbf65b1a977ddca176414
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-12-02 13:30:17 +0700

    working SPI code python

--- Python/spi.py
+++ Python/spi.py
@@ -4,18 +4,37 @@ import spidev
 import time
 
 BUS = 0
-DEV = 1
+DEV = 0
 spi = spidev.SpiDev()
 spi.open(BUS, DEV)      # lihat di /dev/spidev<bus>.<device>
 
-MSG_LENGTH = 5
+MESSAGE_LENGTH = 42
 
-# settings example
+# settings, SPI mode 0, max speed 400kHz
 spi.max_speed_hz = 400000
 spi.mode = 0b00
 
-while True:
-    data = spi.readbytes(5)
-    print(data)
-    time.sleep(3)
+try:
+    while True:
+        start = time.time_ns()
 
+        # placeholder
+        # sent data length == expected data length received from slave
+        buf = [0x00] * MESSAGE_LENGTH
+        buf[0] = 0x10
+
+        data = spi.xfer(buf)
+
+        # Convert returned data to char, and joins them to a string
+        data = "".join([chr(x) for x in data])
+
+        print(data)
+        time_diff = time.time_ns() - start
+
+        # time diff is in nanosecond, convert to second but retaining floating points
+        time_diff /= 1000000000
+        time.sleep(2 - time_diff)
+
+finally:
+    # close spi device before exit
+    spi.close()
