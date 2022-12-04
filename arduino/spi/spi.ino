b326ac1238d4281a339f0f78ae48eec452f7349e
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-12-04 11:53:42 +0700

    arduino spi code works

--- arduino/spi/spi.ino
+++ arduino/spi/spi.ino
@@ -8,8 +8,8 @@
  * SPCR = SPI Control Register, consists of 8 bit value, each controlling SPI functionality
  * |  ------------------------- SPCR ---------------------------- |
  * | SPIE | SPE   | DORD  | MSTR  | CPOL  | CPHA  | SPR1  | SPR0  |
- * |  7   |   6   |   5   |   4   |   3   |   2   |   1   |   0   | 
- * 
+ * |  7   |   6   |   5   |   4   |   3   |   2   |   1   |   0   |
+ *
  * SPIE = enables SPI interrupt when 1
  * SPE  = enables SPE when 1
  * DORD = Sends data least Significant Bit First when 1, most Significant Bit first when 0
@@ -17,13 +17,14 @@
  * CPOL = Sets the data clock to be idle when high if set to 1, idle when low if set to 0
  * CPHA = Samples data on the falling edge of the data clock when 1, rising edge when 0
  * SPR1 and SPR0 - Sets the SPI speed, 00 is fastest (4MHz) 11 is slowest (250KHz)
-*/
+ */
 
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Wire.h>
 #include <MPU6050_light.h>
 #include <SPI.h>
+#include <stdlib.h>
 
 // define constants
 #define LED_PIN PIN_A1
@@ -41,12 +42,13 @@ DHT dht(DHTPIN, DHTTYPE);
 MPU6050 mpu(Wire);
 
 // define vars
-String globalData;
 char cdata[MAX_LENGTH];
 unsigned long timer = 0;
 bool blinkState = false;
 volatile byte pos;
 volatile bool processed;
+float hum, temp, acc[3];
+char chum[7], ctemp[7], cacc0[7], cacc1[7], cacc2[7];
 
 void mpu_setup()
 {
@@ -54,7 +56,11 @@ void mpu_setup()
   byte status = mpu.begin();
   Serial.println(F("MPU6050 status: "));
   Serial.println(status);
-  while (status != 0) { }
+
+  // stop everything if sensor is not ok
+  while (status != 0)
+  {
+  }
 
   Serial.println(F("Calculating offsets, do not move MPU6050..."));
 
@@ -62,30 +68,11 @@ void mpu_setup()
   delay(1000);
 
   mpu.calcOffsets(false, true);
-
-  Serial.println(F("Done MPU setting."));
 }
 
 void dht_setup()
 {
   dht.begin();
-  Serial.println(F("Done dht setting."));
-}
-
-String getData()
-{
-  float hum = dht.readHumidity(); 
-  float temp = dht.readTemperature();
-
-  float acc[3] = {
-    mpu.getAccX(),
-    mpu.getAccY(),
-    mpu.getAccZ()
-  };
-  String data;
-  data = "|h=" + String(hum) + "|t=" + String(temp) + "|x=" + String(acc[0]) + ",y=" + String(acc[1]) + ",z=" + String(acc[2]) + "|";
-  // data = "|h=" + String(hum) + "|t=" + String(temp);
-  return data;
 }
 
 void setup()
@@ -96,7 +83,9 @@ void setup()
   Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
   Serial.println("Begin setup");
   dht_setup();
+  Serial.println(F("Done dht setting."));
   mpu_setup();
+  Serial.println(F("Done MPU setting."));
 
   // these results in SPCR = 0b01000000
   SPCR |= (1 << SPE);
@@ -112,16 +101,16 @@ void setup()
 
   pos = 0;
   processed = false;
-  Serial.println(F("Starting program."));  
+  Serial.println(F("Starting program."));
 }
 
 // SPI interrupt routine
 // called every SPI transfer cycle.
-ISR (SPI_STC_vect)
+ISR(SPI_STC_vect)
 {
   byte b = SPDR;
 
-  // First start byte received from master. 
+  // First start byte received from master.
   // This indicates the begining of SPI transaction.
   if (b == 0x10)
   {
@@ -145,17 +134,28 @@ void loop()
   if (!processed)
   {
     mpu.update();
+  }
+  if (millis() - timer > 1000) // update led every sec
+  {
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
 
-    if (millis() - timer > 2000) // update led every 2 sec
-    {
-      globalData = getData();
-      globalData.toCharArray(cdata, globalData.length() + 1);
+    acc[0] = mpu.getAccX();
+    acc[1] = mpu.getAccY();
+    acc[2] = mpu.getAccZ();
 
-      // blinks to indicate program is running
-      blinkState = !blinkState;
-      digitalWrite(LED_PIN, blinkState);
-      Serial.println(globalData);
-      timer = millis();
-    }
+    dtostrf(hum, 2, 2, chum);
+    dtostrf(temp, 2, 2, ctemp);
+    dtostrf(acc[0], 2, 2, cacc0);
+    dtostrf(acc[1], 2, 2, cacc1);
+    dtostrf(acc[2], 2, 2, cacc2);
+    sprintf(cdata, "|h=%s|t=%s|x=%s,y=%s,z=%s|", chum, ctemp, cacc0, cacc1, cacc2);
+    snprintf()
+    Serial.println(cdata);
+
+    // blinks to indicate program is running
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+    timer = millis();
   }
 }
