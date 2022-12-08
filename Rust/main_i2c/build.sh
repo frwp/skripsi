9629cbf746ca6ae26793da3897c02b96cf7bdef0
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2022-12-08 08:40:55 +0700

    update docker and gitignore

--- Rust/main_i2c/build.sh
+++ Rust/main_i2c/build.sh
@@ -10,18 +10,37 @@ echo "Building package..."
 docker run --rm -v $(pwd):/app rust_cross_compile/armv7
 echo "Building package done."
 
-echo "Transfering executable to Raspberry Pi..."
-ssh -o ConnectTimeout=1 -q pi@192.168.4.2 exit || ssh -o ConnectTimeout=1 -q pi@192.168.8.141 exit
-if [ $? -ne 0 ]
+host1="pi@192.168.8.141"
+host2="pi@192.168.4.2"
+check_connection="ssh -o ConnectTimeout=1 -q"
+
+echo "Checking connection..."
+if $($check_connection $host1 exit)
 then
+    working_host=$host1
+    echo "Host ${host1} works"
+elif $($check_connection $host2 exit)
+then
+    working_host=$host2
+    echo "Host ${host2} works"
+else
     echo "Connection failed, is the Raspberry Pi on or has the IP address changed?"
     exit 1
+fi
+
+echo ""
+echo "Transfering executable to Raspberry Pi..."
+
+if command -v rsync &> /dev/null
+then
+    rsync -P --rsh=ssh $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c ${working_host}:~/skripsi/Rust/main_i2c
 else
-    if command -v rsync &> /dev/null
-    then
-        rsync -P --rsh=ssh $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c pi@192.168.8.141:~/skripsi/Rust/main_i2c
-    else
-        scp $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c pi@192.168.8.141:~/skripsi/Rust/main_i2c
-    fi
+    scp $(pwd)/target/armv7-unknown-linux-gnueabihf/release/main_i2c ${working_host}:~/skripsi/Rust/main_i2c
+fi
+
+if [[ $? -ne 0 ]]; then
+    echo "Transfer failed."
+    return 1
+else
     echo "Operation done."
 fi
