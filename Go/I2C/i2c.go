b370fb5156d0dc5e46b57c8f245e171e202b9e53
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2023-01-23 20:37:43 +0700

    Go i2c: added http request

--- Go/I2C/i2c.go
+++ Go/I2C/i2c.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"time"
 
 	"periph.io/x/conn/v3/i2c"
@@ -28,18 +31,59 @@ func main() {
 	bus.SetSpeed(physic.KiloHertz * 600)
 	device := &i2c.Dev{Addr: SLAVE_ADDRESS, Bus: bus}
 
+	// forever loop
 	for {
+		// start counting time
 		start_time := time.Now()
+
+		// allocate memory for buffer
 		write := []byte{0x10}
 		read := make([]byte, MESSAGE_LENGTH)
 
+		// do i2c transaction
 		if err := device.Tx(write, read); err != nil {
 			log.Fatal(err)
 		}
 
+		// calculate elapsed time
 		elapsed := time.Since(start_time)
-		fmt.Println(read)
-		fmt.Printf("%v\n", string(read[:]))
+
+		// add timestamp to data
+		data := fmt.Sprintf("%v\n", string(read[:]))
+		data = fmt.Sprint(time.Now().Unix()) + "|" + data
+		fmt.Printf("%v\n", data)
+
+		// envelope the data in a post form
+		formData := url.Values{
+			"data": {data},
+			"node": {"node1"},
+		}
+
+		// run in goroutine to not block the loop
+		go sendData("http://192.168.8.134:8080/api", formData)
+
 		time.Sleep(time.Second*2 - elapsed)
 	}
+}
+
+// sendData takes a url and data and sends a post request to the url with the data
+func sendData(url string, data url.Values) {
+	// Create a new post request to the url with the data
+	resp, err := http.PostForm(url, data)
+	// If there was an error, panic
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Close the response body when the function returns
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	// If there was an error, panic
+	if err != nil {
+		log.Panic(err)
+	}
+	// Print the response body to the console
+	log.Println(string(body))
 }
